24089b775c4fd81f57f2df77d1f8d535c667db12
Nhan (Noah) N. TRAN <noah@enclave.vn>
2020-10-14 14:08:36 +0700

    print perty

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,75 +1,93 @@
 
-from colorit import color, Colors, init_colorit
+import os
+from pprint import pprint
+from colorit import color as fg, background as bg
 
-init_colorit()
+os.system("clear")
 
 
-def cl(value):
-    if value != 0:
-        return color(value, Colors.red)
-    return value
+def color(v, h, value):
+    iv = int(v / 3)
+    ih = int(h / 3)
+    return bg(
+        fg(f' {value} ', (0, 0, 0) if value != 0 else (200, 200, 200)),
+        (180, 180, 180) if iv in [0, 2] and ih in [0, 2] or iv == ih == 1 else (140, 140, 140)
+    )
 
 
 class Cell:
     value = 0
     notes = []
     block = False
-    H = 0
-    V = 0
-    h = 0
     v = 0
+    h = 0
 
-    def __init__(self, H, V, value):
-        self.H = H
-        self.V = V
+    def __init__(self, v, h, value):
         self.value = value
         if value != 0:
             self.block = True
         else:
             self.notes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            self.notes.remove(value)
-        self.h = H % 3
-        self.v = V % 3
+        self.v = v
+        self.h = h
 
-    def get_value(self):
-        return self.value
+    def reduce_note(self, value):
+        if value in self.notes:
+            self.notes.remove(value)
 
     def get_pos(self):
-        return f'{self.H}:{self.V}'
+        return f'{self.h}:{self.v}'
 
     def on_border(self, pos):
         return pos if 0 <= pos <= 8 else -1
 
     def cell_on_left(self):
-        return self.on_border(self.H -1)
+        return self.on_border(self.h - 1)
     
     def cell_on_right(self):
-        return self.on_border(self.H + 1)
+        return self.on_border(self.h + 1)
 
     def cell_on_above(self):
-        return self.on_border(self.V - 1)
+        return self.on_border(self.v - 1)
 
     def cell_on_below(self):
-        return self.on_border(self.V + 1)
+        return self.on_border(self.v + 1)
 
 
 class Matrix:
     cells = []
-    sudoku = {}
+    mtx = {}
 
     def __init__(self):
         pass
 
     def add(self, cell):
-        self.sudoku[cell.get_pos()] = cell.get_value()
-        if cell.get_value() != 0:
+        self.mtx[cell.get_pos()] = cell
+        if cell.value != 0:
             self.cells.append(cell)
+
+    def get(self, v, h):
+        return self.mtx[f'{v}:{h}']
+
+    def reduce_note_horizontal(self, cell):
+        if cell.value != 0:
+            for h in range(0, 9):
+                self.get(cell.v, h).reduce_note(cell.value)
+
+    def reduce_note_vertical(self, cell):
+        if cell.value != 0:
+            for v in range(0, 9):
+                self.get(v, cell.h).reduce_note(cell.value)
+
+    def reduce_note_square(self, cell):
+        if cell.value != 0:
+            pass
 
     def print(self):
         for v in range(0, 9):
             for h in range(0, 9):
-                 print(cl(self.sudoku[f'{v}:{h}']), end='\t')
-            print('\n')
+                print(color(v, h, self.get(v, h).value), end='')
+            print()
 
 
 def load_from_file(file_path):
