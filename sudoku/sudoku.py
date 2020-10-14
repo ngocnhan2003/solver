d640de5b726d7aa0de9aa4b43941021cb9991804
Nhan (Noah) N. TRAN <noah@enclave.vn>
2020-10-14 15:50:16 +0700

    reface

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,6 +1,4 @@
-
 import os
-from pprint import pprint
 from colorit import color as fg, background as bg
 
 os.system("clear")
@@ -18,26 +16,21 @@ def color(v, h, value):
 class Cell:
     value = 0
     notes = []
-    lock = False
-    found = False
     v = 0
     h = 0
 
     def __init__(self, v, h, value):
         self.value = value
-        if value != 0:
-            self.lock = True
-        else:
-            self.notes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.v = v
         self.h = h
+        if value == 0:
+            self.notes = list(range(1, 10))
 
     def reduce_note(self, note):
         if note in self.notes:
             self.notes.remove(note)
         if len(self.notes) == 1:
             self.value = self.notes[0]
-            self.found = True
         return self
 
     def __str__(self):
@@ -64,25 +57,15 @@ class Matrix:
 
     def reduce_notes(self):
         for cell in self.cells:
-            if len(self.cells) <= 81:
-                self.reduce_note_horizontal(cell)
-                self.reduce_note_vertical(cell)
-                self.reduce_note_square(cell)
-
-    def reduce_note_horizontal(self, cell):
-        for h in range(0, 9):
-            self.add(self.get(cell.v, h).reduce_note(cell.value))
-
-    def reduce_note_vertical(self, cell):
-        for v in range(0, 9):
-            self.add(self.get(v, cell.h).reduce_note(cell.value))
-
-    def reduce_note_square(self, cell):
-        vi = int(cell.v / 3) * 3
-        hi = int(cell.h / 3) * 3
-        for v in range(0, 3):
-            for h in range(0, 3):
-                self.add(self.get(vi + v, hi + h).reduce_note(cell.value))
+            for h in range(0, 9):
+                self.add(self.get(cell.v, h).reduce_note(cell.value))
+            for v in range(0, 9):
+                self.add(self.get(v, cell.h).reduce_note(cell.value))
+            vi = int(cell.v / 3) * 3
+            hi = int(cell.h / 3) * 3
+            for v in range(0, 3):
+                for h in range(0, 3):
+                    self.add(self.get(vi + v, hi + h).reduce_note(cell.value))
 
     def print(self):
         for v in range(0, 9):
@@ -110,10 +93,10 @@ def load_from_file(file_path):
     return matrix
 
 
-matrix = load_from_file('sudoku.m.input')
+matrix = load_from_file('sudoku.h.input')
 matrix.print()
-# matrix.print_notes()
+matrix.print_notes()
 matrix.reduce_notes()
 print()
-# matrix.print_notes()
+matrix.print_notes()
 matrix.print()
